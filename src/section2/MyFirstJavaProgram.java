29bdbce689d67dea49eb4d9e025c9810f70f2820
League Student <league@iMac-23.attlocal.net>
2019-08-06 14:24:25 -0700

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+		Robot thanos = new Robot();
+		thanos.setPenColor(30, 60, 90);
+		thanos.penDown();
+		thanos.setSpeed(100);
+		for(int i=0;i<4;i++) {
+			thanos.move(200);
+			thanos.turn(90);
 		
+		}
 		
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,30 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
+		Robot lazar = new Robot();
 		
 		// 5. Set your robot's pen down 
+		lazar.penDown();
 		
 		// 3. Set the robot to go at max speed (100)
+		lazar.setSpeed(100);
 		
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i=0;i<75;i++) {
+			lazar.setRandomPenColor();
+			lazar.setPenWidth(i);
+		
 		
 			// 7. Change the pen color to random
 		
 			// 6. Move the robot 5 times the loop counter (5*i)
+			lazar.move(5*i);
 			
 			// 2. Turn the robot 360/7 degrees to the right
+			lazar.turn(360/7);
 		
 			// 8. Set the pen width to i
+		}
 			
 	}
 }
