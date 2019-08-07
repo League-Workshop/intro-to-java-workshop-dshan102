11f8d99a5c2e26268f9c2ae8b23f10c4c84127ce
League Student <league@iMac-23.attlocal.net>
2019-08-07 14:20:34 -0700

    Day 3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,11 +9,21 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+		String word = "spell tralala";
+		speak(word);
+		word = JOptionPane.showInputDialog("type the word");
 
 		// 2. Catch the user's answer in a String
+		String correct = "correct";
+		String incorrect = "wrong";
 
 		// 3. If the user spelled the word correctly, speak "correct"
-
+		if (word.equalsIgnoreCase("tralala")) {
+			speak(correct);
+		}else {
+			speak(incorrect);
+		}
+		
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
